8eaed28cef5831f62b6faaf23fd0a16d56e487d4
Tamira <t.gunzburg@gmail.com>
2021-08-24 13:27:32 +0200

    Refactor insurance calculator utility

--- insuranceCalculator.js
+++ insuranceCalculator.js
@@ -1,20 +1,19 @@
 const calculate = (arrayOfNumbers) => {
   let total = 0;
-  if (Array.isArray(arrayOfNumbers) && arrayOfNumbers) {
-    arrayOfNumbers.forEach((distance) => {
+  if (Array.isArray(arrayOfNumbers) && arrayOfNumbers.length > 0) {
+    return arrayOfNumbers.reduce((accumulator, distance) => {
       if (distance >= 0 && distance <= 20) {
-        total += 200;
-      } else if (distance >= 21 && distance <= 50) {
-        total += 200 + (distance - 20);
-      } else if (distance >= 51 && distance <= 100) {
-        total += 220 + 0.8 * (distance - 50);
+        return (accumulator += 200);
+      } else if (distance <= 50) {
+        return (accumulator += 200 + (distance - 20));
+      } else if (distance <= 100) {
+        return (accumulator += 220 + 0.8 * (distance - 50));
       } else if (distance > 101) {
-        total += 260 + 0.5 * (distance - 100);
+        return (accumulator += 260 + 0.5 * (distance - 100));
       } else {
         throw new Error("Invalid input");
       }
-    });
-    return total;
+    }, 0);
   } else {
     throw new Error("Invalid array input");
   }
